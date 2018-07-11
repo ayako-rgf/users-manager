e245248ed45511f0a73f67db44c444c6f8820019
Ayako Sainoki <ayako@r.recruit.co.jp>
2018-07-11 16:09:14 +0800

    requests: add approve/reject buttons

--- src/app/requests/requests.component.ts
+++ src/app/requests/requests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
 import { MatSort, MatTableDataSource } from '@angular/material';
 import { BeerService } from '../beer.service';
 import { Request } from '../request';
@@ -9,19 +10,65 @@ import { Request } from '../request';
 })
 export class RequestsComponent implements OnInit {
     public dataSource: any;
-    public displayedColumns: string[] = ['id', 'requester', 'subject', 'status'];
+    public displayedColumns: string[] = ['select', 'id', 'requester', 'subject', 'status'];
+    public selection: any;
     @ViewChild(MatSort) sort: MatSort;
 
     constructor (private beerService: BeerService) { }
 
     ngOnInit () {
-        this.getUsers();
+        this.getRequests();
+        this.selection = new SelectionModel<Request>(true, []);
     }
-    public getUsers (): void {
+    public getRequests (): void {
         this.beerService.getRequests()
             .subscribe((users: Request[]) => {
                 this.dataSource = new MatTableDataSource(users);
                 this.dataSource.sort = this.sort;
             });
+    }
+    public isAllSelected (): boolean {
+        const numSelected = this.selection.selected.length;
+        const numRows = this.dataSource.data.length;
+        return numSelected === numRows;
+    }
+    public masterToggle (): void {
+        if (this.isAllSelected()) {
+            this.selection.clear();
+        } else {
+            this.dataSource.data.forEach((row: Request) => this.selection.select(row));
+        }
+    }
+    public onMasterCheckboxChange ($event): void {
+        if ($event) {
+            this.masterToggle();
+        }
+    }
+    public isMasterCheckboxChecked (): boolean {
+        return this.selection.hasValue() && this.isAllSelected();
+    }
+    public isMasterCheckboxIndeterminate (): boolean {
+        return this.selection.hasValue() && !this.isAllSelected();
+    }
+    public onCheckboxChange ($event, row): void {
+        if ($event) {
+            this.selection.toggle(row);
+        }
+    }
+    public isCheckboxChecked (row): boolean {
+        return this.selection.isSelected(row);
+    }
+    public onCheckboxClicked ($event): void {
+        $event.stopPropagation();
+    }
+    public approveSelected ($event): void {
+        $event.preventDefault();
+        $event.stopPropagation();
+        console.log(this.selection.selected);
+    }
+    public rejectSelected ($event): void {
+        $event.preventDefault();
+        $event.stopPropagation();
+        console.log(this.selection.selected);
     }
 }
