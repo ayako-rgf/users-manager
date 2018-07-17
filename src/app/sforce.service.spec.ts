c19129d8a064cd851ba6e8649ca1f56d0c65e9b6
Ayako Sainoki <ayako@r.recruit.co.jp>
2018-07-17 14:27:03 +0800

    initialize sforce service

---
+++ src/app/sforce.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { SforceService } from './sforce.service';
+
+describe('SforceService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [SforceService]
+    });
+  });
+
+  it('should be created', inject([SforceService], (service: SforceService) => {
+    expect(service).toBeTruthy();
+  }));
+});
