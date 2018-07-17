7cdc30b501d63a67c2872cddc59bd33f2ad4cdd4
Ayako Sainoki <ayako@r.recruit.co.jp>
2018-07-17 15:05:43 +0800

    sforce: create DataService instance

--- src/app/oauth/oauth.component.ts
+++ src/app/oauth/oauth.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import DataService from '../forcejs/data-service';
+import { SforceService } from '../sforce.service';
 
 @Component({
     selector: 'app-oauth',
@@ -9,42 +9,9 @@ import DataService from '../forcejs/data-service';
 })
 export class OauthComponent implements OnInit {
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute, private sforceService: SforceService) { }
 
     ngOnInit() {
-        const fragment = this.route.snapshot.fragment;
-        const oauthResult = this.getQueryStringAsObject(fragment);
-        console.log(oauthResult);
-        const settings = this.getSettingsObjectToCreateDataServiceInstance(oauthResult);
-        console.log(settings);
-        const options = {
-            apiVersion: 'v36.0',
-            loginURL: 'https://test.salesforce.com',
-            proxyURL: 'https://ayako-cors-proxy.herokuapp.com/'
-        };
-        const service = DataService.createInstance(settings, options);
-
-        service.query('select id, Name from contact LIMIT 5')
-            .then(response => {
-                console.log(response);
-            });
-    }
-    private getQueryStringAsObject (fragment: string): any {
-        const obj = {};
-        const params = fragment.split('&');
-        params.forEach(param => {
-            const splitter = param.split('=');
-            obj[splitter[0]] = splitter[1];
-        });
-        return obj;
-    }
-    private getSettingsObjectToCreateDataServiceInstance (oauthResult: any): any {
-        return {
-            appId: '3MVG9Se4BnchkASnHuvTyYh3Kq8fpsLhxvnw20rMSBDRWTixsqiAzcTGobwRZcTGN5mZoG7vHDW3MB17gVAU8',
-            accessToken: oauthResult.access_token,
-            instanceURL: oauthResult.instance_url,
-            refreshToken: oauthResult.refresh_token,
-            userId: oauthResult.id.split('/').pop()
-        };
+        this.sforceService.createDataServiceInstance(this.route.snapshot.fragment);
     }
 }
--- src/app/sforce.service.ts
+++ src/app/sforce.service.ts
@@ -6,6 +6,7 @@ import DataService from './forcejs/data-service';
     providedIn: 'root'
 })
 export class SforceService {
+    private forcejsDataService: any;
 
     constructor () { }
 
@@ -15,5 +16,38 @@ export class SforceService {
         const oauthCallbackURL = 'http://localhost:4200/oauth';
         const oauth = OAuth.createInstance(appId, loginURL, oauthCallbackURL);
         oauth.login();
+    }
+    public createDataServiceInstance (oauthResultString: string): void {
+        const oauthResultObject = this.getQueryStringAsObject(oauthResultString);
+        const settings = this.getSettingsObjectToCreateDataServiceInstance(oauthResultObject);
+        const options = {
+            apiVersion: 'v36.0',
+            loginURL: 'https://test.salesforce.com',
+            proxyURL: 'https://ayako-cors-proxy.herokuapp.com/'
+        };
+        const service = DataService.createInstance(settings, options);
+
+        service.query('select id, Name from contact LIMIT 5')
+            .then(response => {
+                console.log(response);
+            });
+    }
+    private getQueryStringAsObject (fragment: string): any {
+        const obj = {};
+        const params = fragment.split('&');
+        params.forEach(param => {
+            const splitter = param.split('=');
+            obj[splitter[0]] = splitter[1];
+        });
+        return obj;
+    }
+    private getSettingsObjectToCreateDataServiceInstance (oauthResult: any): any {
+        return {
+            appId: '3MVG9Se4BnchkASnHuvTyYh3Kq8fpsLhxvnw20rMSBDRWTixsqiAzcTGobwRZcTGN5mZoG7vHDW3MB17gVAU8',
+            accessToken: oauthResult.access_token,
+            instanceURL: oauthResult.instance_url,
+            refreshToken: oauthResult.refresh_token,
+            userId: oauthResult.id.split('/').pop()
+        };
     }
 }
