ae70e85763ca0871d907057dc0a599cfaa3b6ac3
Ayako Sainoki <ayako@r.recruit.co.jp>
2018-07-25 17:58:08 +0800

    add-user: reactive form

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { User } from '../user';
 import { Request } from '../request';
 import { RequestService } from '../request.service';
@@ -12,14 +13,26 @@ import { SforceService } from '../sforce.service';
 })
 export class AddUserComponent implements OnInit {
     public user: User;
+    public userForm: FormGroup;
 
-    constructor (private requestService: RequestService, private sforceService: SforceService, public snackBar: MatSnackBar) { }
+    constructor (private requestService: RequestService, private sforceService: SforceService, public snackBar: MatSnackBar) {
+    }
 
     ngOnInit () {
-        this.resetUser();
+        this.resetForm();
     }
-    private resetUser (): void {
+    private resetForm (): void {
         this.user = new User();
+        this.userForm = this.createFormGroup(this.user);
+    }
+    private createFormGroup (user: User): FormGroup {
+        return new FormGroup({
+            name: new FormControl(user.Name, [Validators.required]),
+            email: new FormControl(user.Email, [
+                Validators.required,
+                Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/)
+            ])
+        });
     }
     public add ($event): void {
         $event.preventDefault();
@@ -38,7 +51,7 @@ export class AddUserComponent implements OnInit {
             const message = 'A new user "' + this.user.Name + '" requested.';
             console.log(message);
             this.openSnackBar(message);
-            this.resetUser();
+            this.resetForm();
         });
     }
     private openSnackBar (message: string): void {
